44cbe8b6f30a40e6d594a986393b6ba22cbbc773
abdullahiolajide <igeabdullahiolajide@gmail.com>
2025-06-10 22:13:30 +0100

    - File reading, writing and appending - using modules - classes and objects

---
+++ classes.py
@@ -0,0 +1,6 @@
+from Student import Student
+
+student1 = Student("Abdullahi", "Business", "4.5", False)
+student2 = Student("Pam", "Art", "2.5", True)
+
+print(student2.name)
---
+++ writing_to_files.py
@@ -0,0 +1,13 @@
+
+
+# employee_file = open("employees.txt", "a")
+
+# employee_file.write("\nKelly - Customer Service ")
+
+# employee_file.close()
+
+employee_file = open("index.html", "w")
+
+employee_file.write("<p>This is HTML</p>")
+
+employee_file.close()
